7370a3cd7199947686eb5b7fae75826642f6a3d1
Thomas Chaigneau <thomas.a.chaigneau@gmail.com>
2018-09-25 18:04:42 +0200

    Okk

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ class App extends Component {
           image= "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c0/Tokyo_Tower_and_around_Skyscrapers.jpg/220px-Tokyo_Tower_and_around_Skyscrapers.jpg"
         />
         <Travel
-          destination="Pris"
+          destination="Paris City Beatch"
           country="France"
           distance="8902"
           image="https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=imgres&cd=&cad=rja&uact=8&ved=2ahUKEwiA1avswdbdAhXrAMAKHS-sDw4QjRx6BAgBEAU&url=https%3A%2F%2Ffr.wikipedia.org%2Fwiki%2FParis&psig=AOvVaw1_c84w5R4Qpxd1dH-N8nau&ust=1537977245112951"
